01733ab2c0aca75abe6593c9ca8410fd52c9d17a
safiruddin <safir798549@gmail.com>
2022-04-14 17:50:30 +0530

    initial commit

---
+++ jenkins2/src/com/jenkin/HelloJenkins.java
@@ -0,0 +1,10 @@
+package com.jenkin;
+
+public class HelloJenkins {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello!,Jenins");
+		System.out.println("Hello! Jenkins It's my first commit on github");
+	}
+
+}
